e1d489dbde47ab53097e87933e1e3fb06a328b7a
Rahul Suresh <rahs98@gmail.com>
2023-05-15 08:39:17 +0530

    App bar with search

--- frontend/src/layouts/Home/index.js
+++ frontend/src/layouts/Home/index.js
@@ -1,4 +1,7 @@
 import "./style.css";
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import SearchIcon from '@mui/icons-material/Search';
 import Grid from '@mui/material/Grid';
 import HomeFeed from "../../components/HomeFeed";
 import HomeProfile from "../../components/HomeProfile";
@@ -6,12 +9,26 @@ import HomeSuggested from "../../components/HomeSuggested";
 // import HomeFavorites from "../../components/HomeFavorites";
 
 const HomeLayout = () => {
-    return <div>
-        <Grid container>
+    return <div className="HomeLayout_container">
+        <div className="HomeLayout_appbar">
+            <span>MediHub</span>
+            <div style={{width: '20px'}}></div>
+            <TextField
+                id="outlined-uncontrolled"
+                size="small"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    ),
+                }}
+                variant="outlined"
+            />
+        </div>
+        <Grid container spacing={0}>
             <Grid item lg={3}>
                <HomeProfile />
-               {/* <div style={{height:'10px'}} />
-               <HomeFavorites /> */}
             </Grid>
             <Grid className="HomeFeed_Grid" item lg={6}>
                 <HomeFeed />

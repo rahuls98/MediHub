d0db10d45833e84d3bbc54b2470c4a1608c802e0
Rahul Suresh <rahs98@gmail.com>
2023-05-15 23:10:53 +0530

    Removed archive

--- frontend/src/archive/HomeFavorites/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const HomeFavorites = () => {
-    return <div className="HomeFavorites_container">
-        HomeFavorites
-    </div>
-}
-
-export default HomeFavorites;
--- frontend/src/archive/HomeSuggested/index.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-const HomeSuggested = () => {
-    return <div className="HomeSuggested_container">HomeSuggested</div>
-}
-
-export default HomeSuggested;

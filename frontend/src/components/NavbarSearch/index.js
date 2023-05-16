8bb8491a375993ac480423698c02c1f1dfd8f91c
Rahul Suresh <rahs98@gmail.com>
2023-05-16 19:08:58 +0530

    medicalAbbreviations

--- frontend/src/components/NavbarSearch/index.js
+++ frontend/src/components/NavbarSearch/index.js
@@ -1,14 +1,17 @@
 import "./style.css";
 import SearchIcon from '@mui/icons-material/Search';
 import medicalDictionary from "../../utils/medicalDictionary";
+import medicalAbbreviations from "../../utils/medicalAbbreviations";
 import Autocomplete from '@mui/joy/Autocomplete';
+
+const searchOptions = [...medicalDictionary, ...medicalAbbreviations];
 
 const NavbarSearch = () => {
     return <div className="NavbarSearch_container">
         <Autocomplete
             placeholder="Search"
             freeSolo
-            options={medicalDictionary}
+            options={searchOptions}
             sx={{ width: 400 }}
             startDecorator={<SearchIcon />}
         />

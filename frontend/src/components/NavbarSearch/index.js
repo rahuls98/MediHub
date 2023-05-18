6ab4bcb55da58f1e57518c11f1138db8b3f3f6ac
Rahul Suresh <rahs98@gmail.com>
2023-05-18 09:10:57 +0530

    Removed autocomplete lag

--- frontend/src/components/NavbarSearch/index.js
+++ frontend/src/components/NavbarSearch/index.js
@@ -2,7 +2,7 @@ import "./style.css";
 import SearchIcon from '@mui/icons-material/Search';
 import medicalDictionary from "../../utils/medicalDictionary";
 import medicalAbbreviations from "../../utils/medicalAbbreviations";
-import Autocomplete from '@mui/joy/Autocomplete';
+import Autocomplete, {createFilterOptions} from '@mui/joy/Autocomplete';
 
 const searchOptions = [...medicalDictionary, ...medicalAbbreviations];
 
@@ -12,6 +12,10 @@ const NavbarSearch = () => {
             placeholder="Search"
             freeSolo
             options={searchOptions}
+            filterOptions={createFilterOptions({
+                matchFrom: 'any',
+                limit: 500,
+            })}
             sx={{ width: 400 }}
             startDecorator={<SearchIcon />}
         />

998a27e925f53931c6ecc4b6bf25aca6a528cfdd
Rahul Suresh <rahs98@gmail.com>
2023-07-10 19:50:29 -0400

    Trending 15

--- backend/src/models/Topic.ts
+++ backend/src/models/Topic.ts
@@ -1,76 +1,72 @@
 import mongoose from "mongoose";
 import TopicSchema from "../schemas/Topic";
 
-const Topic = mongoose.model('Topic', TopicSchema);
+const Topic = mongoose.model("Topic", TopicSchema);
 
-const createTopic = async (
-    title:string
-) => {
+const createTopic = async (title: string) => {
     try {
-        const topic = new Topic({title});
+        const topic = new Topic({ title });
         await topic.save();
     } catch (error) {
-        console.error('Error createTopic: ', error);
+        console.error("Error createTopic: ", error);
     }
-}
+};
 
-const readTopics = async (
-    topicsToExclude:string[]
-) => {
+const readTopics = async (topicsToExclude: string[]) => {
     try {
-        const topics:object[] = await Topic.find({ _id: {$nin: topicsToExclude} })
-            .limit(100);
+        const topics: object[] = await Topic.find({
+            _id: { $nin: topicsToExclude },
+        }).limit(100);
         return topics;
     } catch (error) {
-        console.error('Error readTopics: ', error);
+        console.error("Error readTopics: ", error);
     }
-}
+};
 
-const searchTopics = async (
-    searchCriteria:string
-) => {
+const searchTopics = async (searchCriteria: string) => {
     try {
         const regex = new RegExp(searchCriteria, "i");
-        const topics:object[] = await Topic.find({ title: { $regex: regex } })
-            .limit(100);
+        const topics: object[] = await Topic.find({
+            title: { $regex: regex },
+        }).limit(100);
         return topics;
     } catch (error) {
-        console.error('Error searchTopics: ', error);
+        console.error("Error searchTopics: ", error);
     }
-}
+};
 
 const readTrendingTopics = async () => {
     try {
-        const topics:object[] = await Topic.find()
+        const topics: object[] = await Topic.find()
             .sort({ searchCounter: -1 })
-            .limit(10);
+            .limit(15);
         return topics;
     } catch (error) {
-        console.error('Error readTrendingTopics: ', error);
+        console.error("Error readTrendingTopics: ", error);
     }
-}
+};
 
-const readTopicsById = async (
-    topicIds:string[]
-) => {
+const readTopicsById = async (topicIds: string[]) => {
     try {
-        const objectIdTopicIds = topicIds.map(id => new mongoose.Types.ObjectId(id));
-        const topics:object[] = await Topic.find({ _id: { "$in": objectIdTopicIds } });
+        const objectIdTopicIds = topicIds.map(
+            (id) => new mongoose.Types.ObjectId(id)
+        );
+        const topics: object[] = await Topic.find({
+            _id: { $in: objectIdTopicIds },
+        });
         return topics;
     } catch (error) {
-        console.error('Error readTopicsById: ', error);
+        console.error("Error readTopicsById: ", error);
     }
-}
+};
 
-const incrementSeachCounter = async (
-    topic:string
-) => {
+const incrementSeachCounter = async (topic: string) => {
     try {
-        await Topic.updateOne({ title: topic }, {$inc : {searchCounter : 1}})
+        await Topic.updateOne({ title: topic }, { $inc: { searchCounter: 1 } });
     } catch (error) {
-        console.error('Error incrementSeachCounter: ', error);
+        console.error("Error incrementSeachCounter: ", error);
     }
-}
+};
 
 const TopicModel = {
     createTopic,
@@ -78,7 +74,7 @@ const TopicModel = {
     searchTopics,
     readTrendingTopics,
     readTopicsById,
-    incrementSeachCounter
+    incrementSeachCounter,
 };
 
 export default TopicModel;
--- frontend/src/components/FeedTrending/index.js
+++ frontend/src/components/FeedTrending/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./style.css";
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import Stack from '@mui/material/Stack';
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import Stack from "@mui/material/Stack";
 import TopicChip from "../TopicChip";
 import feedApis from "../../apis/feed";
 
@@ -12,21 +12,23 @@ const FeedTrending = () => {
         const getTrendingTopics = async () => {
             const trendingTopics = await feedApis.getTrendingTopics();
             setTrendingTopics(trendingTopics.topics);
-        }
+        };
         getTrendingTopics();
-    }, [])
+    }, []);
 
-    return <div className="FeedTrending_container">
-        <div className="FeedTrending_header">
-            <span>Trending 30</span>
-            <TrendingUpIcon sx={{ fontSize: 16 }} />
+    return (
+        <div className="FeedTrending_container">
+            <div className="FeedTrending_header">
+                <span>Trending 15</span>
+                <TrendingUpIcon sx={{ fontSize: 16 }} />
+            </div>
+            <Stack direction="row" flexWrap="wrap">
+                {trendingTopics?.map((topic) => (
+                    <TopicChip key={topic._id} label={topic.title} withMargin />
+                ))}
+            </Stack>
         </div>
-        <Stack direction="row" flexWrap="wrap">
-            {
-                trendingTopics?.map(topic => <TopicChip key={topic._id} label={topic.title} withMargin/>)
-            }
-        </Stack>
-    </div>
-}
+    );
+};
 
 export default FeedTrending;
